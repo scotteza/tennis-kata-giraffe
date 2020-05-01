eb361fd2fa9610b36cf65cf767f14b49c2f4e452
Scott Edwards <scotteza@gmail.com>
2020-05-01 15:14:41 +0100

    Implement basic cases with lots of sliming

---
+++ Tennis.Test/TennisGameShould.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+
+namespace Tennis.Test
+{
+    internal class TennisGameShould
+    {
+        private TennisGame _tennisGame;
+
+        [SetUp]
+        public void Setup()
+        {
+            _tennisGame = new TennisGame();
+        }
+
+        [Test]
+        public void Drive_The_Testing_Framework()
+        {
+            Assert.That(true, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Start_With_A_Score_Of_Love_All()
+        {
+            var score = _tennisGame.GetScore();
+
+            Assert.That(score, Is.EqualTo("love-all"));
+        }
+
+        [Test]
+        public void Let_Player_1_Score_A_Single_Point()
+        {
+            _tennisGame.ScorePoint(1);
+            var score = _tennisGame.GetScore();
+
+            Assert.That(score, Is.EqualTo("fifteen-love"));
+        }
+
+        [Test]
+        public void Let_Player_1_Score_2_Points()
+        {
+            _tennisGame.ScorePoint(1);
+            _tennisGame.ScorePoint(1);
+            var score = _tennisGame.GetScore();
+
+            Assert.That(score, Is.EqualTo("thirty-love"));
+        }
+
+        [Test]
+        public void Let_Player_1_Score_3_Points()
+        {
+            _tennisGame.ScorePoint(1);
+            _tennisGame.ScorePoint(1);
+            _tennisGame.ScorePoint(1);
+            var score = _tennisGame.GetScore();
+
+            Assert.That(score, Is.EqualTo("forty-love"));
+        }
+
+        [Test]
+        public void Let_Both_Players_Score_A_Single_Point()
+        {
+            _tennisGame.ScorePoint(1);
+            _tennisGame.ScorePoint(2);
+            var score = _tennisGame.GetScore();
+
+            Assert.That(score, Is.EqualTo("fifteen-fifteen"));
+        }
+    }
+}
---
+++ Tennis/TennisGame.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Tennis
+{
+    public class TennisGame
+    {
+        private int _player1Points;
+        private int _player2Points;
+
+        public string GetScore()
+        {
+            if (_player1Points == 1 && _player2Points == 1)
+            {
+                return "fifteen-fifteen";
+            }
+
+            if (_player1Points == 1)
+            {
+                return "fifteen-love";
+            }
+
+            if (_player1Points == 2)
+            {
+                return "thirty-love";
+            }
+
+            Dictionary<int, string> scoreLookup = new Dictionary<int, string>();
+            scoreLookup.Add(3, "forty");
+
+            if (_player1Points == 3)
+            {
+                return "forty-love";
+            }
+
+            return "love-all";
+        }
+
+        public void ScorePoint(int playerNumber)
+        {
+            if (playerNumber == 1)
+            {
+                _player1Points++;
+            }
+
+            if (playerNumber == 2)
+            {
+                _player2Points++;
+            }
+        }
+    }
+}
